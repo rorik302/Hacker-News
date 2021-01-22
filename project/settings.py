fdcc5ed1f2f06ab5951db8cc2d453bd76d120219
Aleksandr Danilenko <rorik302@gmail.com>
2021-01-22 20:49:07 +0300

    engine env

--- project/settings.py
+++ project/settings.py
@@ -58,12 +58,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.mysql'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT')
+        'PORT': os.environ.get('DB_PORT', 3306)
     }
 }
 
